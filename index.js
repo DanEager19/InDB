830904f2b5427917241af9cd3fe0792000b0a3c7
Dan Eager <81655709+DanEager19@users.noreply.github.com>
2021-10-19 10:18:07 -0500

    Added HTTP Server

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-const {Express} = require('express');
-const {MongoClient} = require('mongodb');
+const express = require('express');
+//const {MongoClient} = require('mongodb');
+const app = express();
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+    console.log(__dirname);
+    res.sendFile(__dirname + "/index.html");
+})

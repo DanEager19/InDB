bd947f8a4ec1e85ed5518febaf8e8fcb47214f61
Dan Eager <81655709+DanEager19@users.noreply.github.com>
2021-10-25 13:47:49 -0500

    Improved linting

--- model/connect_db.js
+++ model/connect_db.js
@@ -1,19 +1,19 @@
-global.TextEncoder = require("util").TextEncoder;
-global.TextDecoder = require("util").TextDecoder;
-const {MongoClient} = require('mongodb');
+global.TextEncoder = require("util").TextEncoder
+global.TextDecoder = require("util").TextDecoder
+const {MongoClient} = require('mongodb')
 
-const uri = "mongodb+srv://daniel:G9l$23mo0@cluster0.xcoys.mongodb.net/index?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+const uri = 'mongodb+srv://daniel:G9l$23mo0@cluster0.xcoys.mongodb.net/index?retryWrites=true&w=majority'
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 
-async function main() {
-    try {
-        await client.connect();
-        await listDatabases(client);
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
+async function main () {
+  try {
+    await client.connect()
+    await listDatabases(client)
+  } catch (e) {
+    console.error(e)
+  } finally {
+    await client.close()
+  }
 }
 
 async function listDatabases(client) {

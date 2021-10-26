3f5d2be54e6d201fa61dca087ea9d157876f23e4
Dan Eager <81655709+DanEager19@users.noreply.github.com>
2021-10-26 13:16:58 -0500

    Add files via upload

---
+++ src/Head.js
@@ -0,0 +1,13 @@
+
+function Head() {
+  return (
+    <header id="header" class="LayoutTop">
+        <a href="index.html"><img src="/images/logo.png" id="logo" alt="This is the logo." /></a>
+        <a class="text_remove" href="/view/index.html">Home</a> 
+        <a class="text_remove" href="/view/list.html">List</a>  
+        <a class="text_remove" href="/view/login.html">Login</a>
+    </header>
+  )
+}
+
+export default Head;

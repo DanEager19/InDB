a4e7250d475e3b37f4ab81d56ed9b4f03d86d0a7
Dan Eager <81655709+DanEager19@users.noreply.github.com>
2021-10-26 13:24:06 -0500

    React Update

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
+global.TextEncoder = require('util').TextEncoder
+global.TextDecoder = require('util').TextDecoder
 const express = require('express')
-// const {MongoClient} = require('mongodb');
 const app = express()
+const bodyParser = require('body-parser')
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const { MongoClient } = require('mongodb')
 
-const hostname = '127.0.0.1'
-const port = 3000
+const uri = 'mongodb://www.in-db.net:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false'
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 
-app.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
-})
+const database = client.db('index')
+const games = database.collection('games')
+
+let doc = {}
+
+async function insertEntry () {
+  try {
+    await client.connect()
+    await games.insertOne(doc)
+    console.log('Entry sent')
+  } catch (e) {
+    console.error(e)
+  } finally {
+    await client.close()
+  }
+}
 
 app.use(express.static(__dirname))
 
 app.get('/', (req, res) => {
-  console.log(__dirname)
-  res.sendFile(__dirname + "/view/index.html")
+  res.sendFile(__dirname + '/view/add_entry.html')
 })
+
+app.post('/', urlencodedParser, (req, res) => {
+  doc = {
+    title: req.body.title,
+    summary: req.body.summary,
+    img: '',
+    developer: req.body.dev,
+    publisher: req.body.pub,
+    release_date: req.body.date,
+    genres: [req.body.genres],
+    rating: req.body.rating,
+    os: req.body.os,
+    processor: req.body.cpu,
+    memory: req.body.ram,
+    graphics: req.body.gpu,
+    storage: req.body.storage
+  }
+  insertEntry().catch(console.error)
+  res.sendFile(__dirname + '/view/index.html')
+})
+
+app.listen(3000)

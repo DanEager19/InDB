560b1c935b6edd26dc00729b33be9995b6338360
Dan Eager <81655709+DanEager19@users.noreply.github.com>
2021-10-28 08:42:04 -0500

    Moved app to its own file

--- src/index.js
+++ src/index.js
@@ -1,38 +1,7 @@
 //Dependencies
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
-
-//Stylesheets
-import 'bootstrap/dist/css/bootstrap.css'
-import "./index.css"
-
-//Components
-import Navigation from "./components/navbar"
-import Footer from "./components/footer"
-
-//Pages
-import Add_entry from "./pages/add_entry"
-import List from "./pages/list"
-import Login from "./pages/login"
-import Home from './pages/home'
-
-function App() {
-    return(
-        <div>
-            <Router>
-                <Navigation></Navigation>
-                <Switch>
-                    <Route exact path='/' component={Home} />
-                    <Route exact path='/list' component={List} />
-                    <Route exact path='/login' component={Login} />
-                    <Route exact path='/add_entry' component={Add_entry} />
-                </Switch>
-            </Router>
-            <Footer />
-        </div>
-    );
-}
+import App from './App'
 
 ReactDOM.render(
     <App />, 

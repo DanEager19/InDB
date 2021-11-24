26e278a0280acc51cedf9fb706de4d2a569f0a2c
Dan Eager <81655709+DanEager19@users.noreply.github.com>
2021-11-24 17:06:59 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 //Dependencies
+<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 //App
+=======
+import React from 'react'
+import ReactDOM from 'react-dom'
+>>>>>>> parent of 5dd9bbc (Move modal)
 import App from './App'
 
 ReactDOM.render(

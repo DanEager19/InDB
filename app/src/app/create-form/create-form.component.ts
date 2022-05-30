62031b09eb8c8dc4c3d797be178d7eecefc14de2
Dan Eager <81655709+DanEager19@users.noreply.github.com>
2022-05-30 15:51:18 -0500

    updates

--- app/src/app/create-form/create-form.component.ts
+++ app/src/app/create-form/create-form.component.ts
@@ -9,6 +9,24 @@ export class CreateFormComponent implements OnInit {
 
   constructor() { }
 
+  handleChange = (event:any) => {    
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+  handleSubmit = (event:any) => {
+    event.preventDefault()
+
+    const url = 'http://localhost:5000/games'
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(inputs)
+    }
+    fetch(url, requestOptions)
+    .then(() => console.log('Submitted successfully'))
+    .catch(error => console.log('Form submit error', error))
+  };
   ngOnInit(): void {
   }
 
